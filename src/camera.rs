652ce7f0c75f5b38c59f0ad7f0c407f081395bb8
zhanghongtu.zht <zhanghongtu.zht@bytedance.com>
2025-02-26 13:55:40 +0800

    finish

--- src/camera.rs
+++ src/camera.rs
@@ -4,7 +4,7 @@ use crate::{
     interval::Interval,
     ray::Ray,
     rtweekend::{degrees_to_radians, random_f64},
-    vec3::{Point3, Vec3, unit_vector},
+    vec3::{Point3, Vec3, random_in_unit_disk, unit_vector},
 };
 
 pub struct CameraBuilder {
@@ -13,6 +13,11 @@ pub struct CameraBuilder {
     pub samples_per_pixel: u32,
     pub max_depth: u32,
     pub vfov: f64,
+    pub lookfrom: Point3,
+    pub lookat: Point3,
+    pub vup: Vec3,
+    pub defocus_angle: f64,
+    pub focus_dist: f64,
 }
 
 impl Default for CameraBuilder {
@@ -23,6 +28,11 @@ impl Default for CameraBuilder {
             samples_per_pixel: 10,
             max_depth: 10,
             vfov: 90.0,
+            lookfrom: Point3::new(0.0, 0.0, 0.0),
+            lookat: Point3::new(0.0, 0.0, -1.0),
+            vup: Vec3::new(0.0, 1.0, 0.0),
+            defocus_angle: 0.0,
+            focus_dist: 10.0,
         }
     }
 }
@@ -53,39 +63,78 @@ impl CameraBuilder {
         self
     }
 
+    pub fn lookfrom(mut self, lookfrom: &Point3) -> CameraBuilder {
+        self.lookfrom = *lookfrom;
+        self
+    }
+
+    pub fn lookat(mut self, lookat: &Point3) -> CameraBuilder {
+        self.lookat = *lookat;
+        self
+    }
+
+    pub fn vup(mut self, vup: &Vec3) -> CameraBuilder {
+        self.vup = *vup;
+        self
+    }
+
+    pub fn defocus_angle(mut self, defocus_angle: f64) -> CameraBuilder {
+        self.defocus_angle = defocus_angle;
+        self
+    }
+
+    pub fn focus_dist(mut self, focus_dist: f64) -> CameraBuilder {
+        self.focus_dist = focus_dist;
+        self
+    }
+
     pub fn build(&self) -> Camera {
         let aspect_ratio = self.aspect_ratio;
         let image_width = self.image_width;
         let samples_per_pixel = self.samples_per_pixel;
         let max_depth = self.max_depth;
         let vfov = self.vfov;
+        let lookfrom = self.lookfrom;
+        let lookat = self.lookat;
+        let vup = self.vup;
+        let defocus_angle = self.defocus_angle;
+        let focus_dist = self.focus_dist;
 
         // image
         let image_height = (image_width as f64 / aspect_ratio) as u32;
         let image_height = if image_height < 1 { 1 } else { image_height };
 
-        let center = Point3::new(0.0, 0.0, 0.0);
+        let center = lookfrom;
 
         // Determine viewport dimensions
-        let focal_length = 1.0;
+        // let focal_length = (lookfrom - lookat).length();
         let theta = degrees_to_radians(vfov);
         let h = (theta / 2.0).tan();
-        let viewport_height = 2.0 * h * focal_length;
+        let viewport_height = 2.0 * h * focus_dist;
         let real_aspet_ratio = image_width as f64 / image_height as f64;
         let viewport_wideth = viewport_height * real_aspet_ratio;
 
+        // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
+        let w = unit_vector(&(lookfrom - lookat));
+        let u = unit_vector(&vup.cross(&w));
+        let v = w.cross(&u);
+
         // Calculate the vectors across the horizontal and down the vertical viewport edges.
-        let viewport_u = Vec3::new(viewport_wideth, 0.0, 0.0);
-        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+        let viewport_u = viewport_wideth * u;
+        let viewport_v = viewport_height * -v;
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel.
         let pixel_delta_u = viewport_u / image_width as f64;
         let pixel_delta_v = viewport_v / image_height as f64;
 
         // location of upper left pixel
-        let viewport_upper_left =
-            center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+        let viewport_upper_left = center - (focus_dist * w) - viewport_u / 2.0 - viewport_v / 2.0;
         let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        // Calculate the camera defocus disk basis vectors.
+        let defocus_radius = focus_dist * degrees_to_radians(defocus_angle / 2.0).tan();
+        let defocus_disk_u = u * defocus_radius;
+        let defocus_disk_v = v * defocus_radius;
 
         Camera {
             aspect_ratio,
@@ -93,12 +142,22 @@ impl CameraBuilder {
             samples_per_pixel,
             max_depth,
             vfov,
+            lookfrom,
+            lookat,
+            vup,
+            defocus_angle,
+            focus_dist,
             image_height,
             pixel_samples_scale: 1.0 / samples_per_pixel as f64,
             center,
             pixel00_loc,
             pixel_delta_u,
             pixel_delta_v,
+            u,
+            v,
+            w,
+            defocus_disk_u,
+            defocus_disk_v,
         }
     }
 }
@@ -109,13 +168,22 @@ pub struct Camera {
     pub samples_per_pixel: u32,
     pub max_depth: u32,
     pub vfov: f64,
-
+    lookfrom: Point3,
+    lookat: Point3,
+    vup: Vec3,
+    defocus_angle: f64,
+    focus_dist: f64,
     image_height: u32,
     pixel_samples_scale: f64,
     center: Point3,
     pixel00_loc: Point3,
     pixel_delta_u: Vec3,
     pixel_delta_v: Vec3,
+    u: Vec3,
+    v: Vec3,
+    w: Vec3,
+    defocus_disk_u: Vec3,
+    defocus_disk_v: Vec3,
 }
 
 impl Camera {
@@ -148,7 +216,11 @@ impl Camera {
             + ((i as f64 + offset.x()) * self.pixel_delta_u)
             + ((j as f64 + offset.y()) * self.pixel_delta_v);
 
-        let ray_origin = self.center;
+        let ray_origin = if self.defocus_angle <= 0.0 {
+            self.center
+        } else {
+            self.defocus_disk_sample()
+        };
         let ray_dir = pixel_sample - ray_origin;
 
         Ray::new(&ray_origin, &ray_dir)
@@ -156,6 +228,11 @@ impl Camera {
 
     fn sample_square() -> Vec3 {
         Vec3::new(random_f64() - 0.5, random_f64() - 0.5, 0.0)
+    }
+
+    fn defocus_disk_sample(&self) -> Point3 {
+        let p = random_in_unit_disk();
+        self.center + (p.x() * self.defocus_disk_u) + (p.y() * self.defocus_disk_v)
     }
 
     fn ray_color(&self, r: &Ray, depth: u32, world: &impl Hittable) -> Color {
--- src/main.rs
+++ src/main.rs
@@ -13,70 +13,84 @@ use camera::Camera;
 use color::Color;
 use hittable_list::HittableList;
 use material::{Dielectric, Lambertian, Material, Metal};
+use rtweekend::{random_f64, random_f64_within};
 use sphere::Sphere;
 use std::rc::Rc;
 use vec3::{Point3, Vec3};
-use std::f64::consts::PI;
 
 fn main() {
     let mut world = HittableList::default();
 
-    let R = (PI / 4.0).cos();
-    let material_left: Option<Rc<dyn Material>> = Some(Rc::new(Lambertian::new(&Color::new(0.0, 0.0, 1.0))));
-    let material_right: Option<Rc<dyn Material>> = Some(Rc::new(Lambertian::new(&Color::new(1.0, 0.0, 0.0))));
-
-    world.add(Rc::new(Sphere::new(
-        &Point3::new(-R, 0.0, -1.0),
-        R,
-        material_left,
-    )));
-    world.add(Rc::new(Sphere::new(
-        &Point3::new(R, 0.0, -1.0),
-        R,
-        material_right,
-    )));
-
     let material_ground: Option<Rc<dyn Material>> =
-        Some(Rc::new(Lambertian::new(&Color(Vec3::new(0.8, 0.8, 0.0)))));
-    let material_center: Option<Rc<dyn Material>> =
-        Some(Rc::new(Lambertian::new(&Color(Vec3::new(0.1, 0.2, 0.5)))));
-    let material_left: Option<Rc<dyn Material>> = Some(Rc::new(Dielectric::new(1.50)));
-    let material_bubble: Option<Rc<dyn Material>> = Some(Rc::new(Dielectric::new(1.00 / 1.50)));
-    let material_right: Option<Rc<dyn Material>> =
-        Some(Rc::new(Metal::new(&Color(Vec3::new(0.8, 0.6, 0.2)), 1.0)));
-
+        Some(Rc::new(Lambertian::new(&Color(Vec3::new(0.5, 0.5, 0.5)))));
     world.add(Rc::new(Sphere::new(
-        &Point3::new(0.0, -100.5, -1.0),
-        100.0,
+        &Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
         material_ground,
     )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = random_f64();
+            let center = Point3::new(
+                a as f64 + 0.9 * random_f64(),
+                0.2,
+                b as f64 + 0.9 * random_f64(),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                let sphere_material: Option<Rc<dyn Material>> = {
+                    if choose_mat < 0.8 {
+                        let albedo = Color(Vec3::random()) * Color(Vec3::random());
+                        Some(Rc::new(Lambertian::new(&albedo)))
+                    } else if choose_mat < 0.95 {
+                        let albedo = Color(Vec3::random_within(0.5, 1.0));
+                        let fuzz = random_f64_within(0.0, 0.5);
+                        Some(Rc::new(Metal::new(&albedo, fuzz)))
+                    } else {
+                        Some(Rc::new(Dielectric::new(1.5)))
+                    }
+                };
+
+                world.add(Rc::new(Sphere::new(&center, 0.2, sphere_material)));
+            }
+        }
+    }
+
+    let material1: Option<Rc<dyn Material>> = Some(Rc::new(Dielectric::new(1.50)));
     world.add(Rc::new(Sphere::new(
-        &Point3::new(0.0, 0.0, -1.2),
-        0.5,
-        material_center,
+        &Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
     )));
+
+    let material2: Option<Rc<dyn Material>> =
+        Some(Rc::new(Lambertian::new(&Color(Vec3::new(0.4, 0.2, 0.1)))));
     world.add(Rc::new(Sphere::new(
-        &Point3::new(-1.0, 0.0, -1.0),
-        0.5,
-        material_left,
+        &Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
     )));
+
+    let material3: Option<Rc<dyn Material>> =
+        Some(Rc::new(Metal::new(&Color(Vec3::new(0.7, 0.6, 0.5)), 0.0)));
     world.add(Rc::new(Sphere::new(
-        &Point3::new(-1.0, 0.0, -1.0),
-        0.4,
-        material_bubble,
-    )));
-    world.add(Rc::new(Sphere::new(
-        &Point3::new(1.0, 0.0, -1.0),
-        0.5,
-        material_right,
+        &Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
     )));
 
     let mut camera = Camera::builder()
         .aspect_ration(16.0 / 9.0)
-        .image_width(400)
-        .samples_per_pixel(100)
+        .image_width(1200)
+        .samples_per_pixel(500)
         .max_depth(50)
-        .vfov(90.0)
+        .vfov(20.0)
+        .lookfrom(&Point3::new(13.0, 2.0, 3.0))
+        .lookat(&Point3::new(0.0, 0.0, 0.0))
+        .vup(&Vec3::new(0.0, 1.0, 0.0))
+        .defocus_angle(0.6)
+        .focus_dist(10.0)
         .build();
 
     camera.render(&world);
